905c23d7239bc2dca99d86ca9b8f87d334285172
Bala Kamal Teja Thiruveedhula <balakamal22@gmail.com>
2021-06-28 13:06:41 +0530

    commit

---
+++ Vidly/Controllers/api/CustomersController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.DTOS;
+using AutoMapper;
+
+
+namespace Vidly.Controllers.api
+{
+    public class CustomersController : ApiController
+    {
+        private VidlyDbContext _dbContext;
+        public CustomersController()
+        {
+            _dbContext = new VidlyDbContext();
+        }
+
+        //GET /api/Customers
+        public IHttpActionResult GetCustomers()
+        {
+            return Ok(_dbContext.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>));
+        }
+
+        //GET /api/Customers/id
+        public IHttpActionResult GetCustomers(int id)
+        {
+            Customer customer = _dbContext.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return NotFound();
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+        //POST /api/Customers(Store Customer Details in Database)
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            Customer customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _dbContext.Customers.Add(customer);
+            _dbContext.SaveChanges();
+            customerDto.Id = customer.Id;
+            return Created( new Uri(Request.RequestUri + "/" + customerDto.Id),customerDto);
+        }
+
+        //PUT /api/Customers/Id(update customer details)
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            Customer customer = _dbContext.Customers.Find(id);
+            if (customer == null)
+                return NotFound();
+            Mapper.Map<CustomerDto, Customer>(customerDto, customer);
+            _dbContext.SaveChanges();
+            return Ok(customerDto);
+        }
+        //DELETE api/Customers/id
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            Customer customer = _dbContext.Customers.Find(id);
+            if (customer == null)
+                return NotFound();
+            _dbContext.Customers.Remove(customer);
+            _dbContext.SaveChanges();
+            return Ok("Deleted Successfully");
+        }
+    }
+}
---
+++ Vidly/DTOS/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.DTOS
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public bool IsSubscribed { get; set; }
+
+        //[IsAbove18Years]
+        public DateTime? DateOfBirth { get; set; }
+        public byte MembershipTypeId { get; set; }
+    }
+}
